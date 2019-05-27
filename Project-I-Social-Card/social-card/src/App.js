f409f2d435befc3f0a8cb3a059a55792aabe656f
Haja-andri <haja.andri@gmail.com>
2019-05-27 17:02:25 +0200

    finished basic composition of header

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,23 +1,45 @@
 import React from 'react';
 import './App.css';
 
-const App = () => {
+import './components/HeaderComponents/HeaderContent.js'
+
+// starting with the smallest piece
+// header title
+function HeaderTitle ({headerText}){
+  return (
+    <h1>{headerText} <span>@LambdaSchool . 27 may</span></h1>
+  );
+}
+// The header Thumbnail
+function HeaderThumb ({link}) {
+  return (
+    <img src={link} className="logo" />
+  );
+}
+
+// The content
+function HeaderContent ({headerText}) {
   return (
     <div>
-      <h3>Welcome to React Social Card!</h3>
-      <p>
-        Begin by exploring the `components` directory. You'll notice we have a
-        few files that we've already included in there to get you started right
-        away building components. You'll need to make sure you include your
-        components that you build in this file to watch your app come to life
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+      <h1>Lamda School <span>@LambdaSchool . 27 may</span></h1>
+      <p> {headerText} </p>
     </div>
+  );
+}
+function BuildHeader () {
+  return (
+    <div className = "headerContainer">
+      <HeaderThumb link="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png" />
+      <HeaderContent headerText="Let's learn react by builing simple interfaces components.
+      Don't try to overthink it, just keep it simple abd have fun. Once you feel comfortable using
+      components you are well on your way to master React !"/>     
+    </div>
+  );
+}
+
+const App = () => {
+  return (
+    <BuildHeader />
   );
 };
 
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Header.css';
+
+// The content
+function HeaderContent ({headerText}) {
+    return (
+      <p> {headerText} </p>
+    );
+  }
+
+  export default HeaderContent;
