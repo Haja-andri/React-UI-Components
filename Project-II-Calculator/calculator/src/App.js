f8421b4ced1125b1e1d3c9c31f3fc9d83a088f01
Haja-andri <haja.andri@gmail.com>
2019-05-28 18:08:52 +0200

    Separated the code with an additiona file for Number Button

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,50 +1,41 @@
 import React from 'react';
 import './App.css';
+import NumberButton from './components/ButtonComponents/NumberButton'
 
 // The row-child components
 
-function RowChild (props) {
-  const { className, text, bkg } = props;
-
-  return (
-    <div className={className} style={{backgroundColor: `${bkg}`}} >
-    {text}
-    </div>
-  );
-}
-
-
 const App = () => {
+
   return (
     <div className="mainContainer">
       <div className="row">
-        <RowChild className="row-child full" text="0" bkg="black" />
+          <NumberButton className="row-child full" text="0" bkg="black" />
       </div>
       <div className="row">
-        <RowChild className="row-child TF small" text="clear" />
-        <RowChild className="row-child forth white" text="/" bkg="red"/>
+        <NumberButton className="row-child TF small" text="clear" />
+        <NumberButton className="row-child forth white" text="/" bkg="red"/>
       </div>      
       <div className="row">
-        <RowChild className="row-child forth" text="7" />
-        <RowChild className="row-child forth" text="8" />
-        <RowChild className="row-child forth" text="9" />
-        <RowChild className="row-child forth white" text="X" bkg="red" />
+        <NumberButton className="row-child forth" text="7" />
+        <NumberButton className="row-child forth" text="8" />
+        <NumberButton className="row-child forth" text="9" />
+        <NumberButton className="row-child forth white" text="X" bkg="red" />
       </div>      
       <div className="row">
-        <RowChild className="row-child forth" text="4" />
-        <RowChild className="row-child forth" text="5" />
-        <RowChild className="row-child forth" text="6" />
-        <RowChild className="row-child forth white" text="-" bkg="red" />
+        <NumberButton className="row-child forth" text="4" />
+        <NumberButton className="row-child forth" text="5" />
+        <NumberButton className="row-child forth" text="6" />
+        <NumberButton className="row-child forth white" text="-" bkg="red" />
       </div>      
       <div className="row">
-        <RowChild className="row-child forth" text="1" />
-        <RowChild className="row-child forth" text="2" />
-        <RowChild className="row-child forth" text="3" />
-        <RowChild className="row-child forth white" text="+" bkg="red" />
+        <NumberButton className="row-child forth" text="1" />
+        <NumberButton className="row-child forth" text="2" />
+        <NumberButton className="row-child forth" text="3" />
+        <NumberButton className="row-child forth white" text="+" bkg="red" />
       </div>      
       <div className="row">
-        <RowChild className="row-child TF" text="0" />
-        <RowChild className="row-child forth white" text="=" bkg="red"/>
+        <NumberButton className="row-child TF" text="0" />
+        <NumberButton className="row-child forth white" text="=" bkg="red"/>
       </div>      
     </div>
   );
