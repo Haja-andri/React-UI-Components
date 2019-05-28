ae34bfd41258270889091ee2f72ae057caac1fe3
Haja-andri <haja.andri@gmail.com>
2019-05-28 16:34:00 +0200

    Finished basic factoring and rendering

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,23 +1,49 @@
 import React from 'react';
 import './App.css';
 
+// The row-child components
+
+function RowChild (props) {
+  const { className, text, bkg } = props;
+
+  return (
+    <div className={className} style={{backgroundColor: `${bkg}` }} >
+    {text}
+    </div>
+  );
+}
+
 const App = () => {
   return (
     <div className="mainContainer">
       <div className="row">
-        <div className="row-child">
-        I am child
-        </div>
+        <RowChild className="row-child full" text="0" />
       </div>
       <div className="row">
-        <div className="row-child">
-        I am child
-      </div>
+        <RowChild className="row-child TF" text="clear" />
+        <RowChild className="row-child forth" text="/" bkg="red"/>
       </div>      
       <div className="row">
-        <div className="row-child">
-        I am child
-        </div>
+        <RowChild className="row-child forth" text="7" />
+        <RowChild className="row-child forth" text="8" />
+        <RowChild className="row-child forth" text="9" />
+        <RowChild className="row-child forth" text="X" bkg="red" />
+      </div>      
+      <div className="row">
+        <RowChild className="row-child forth" text="4" />
+        <RowChild className="row-child forth" text="5" />
+        <RowChild className="row-child forth" text="6" />
+        <RowChild className="row-child forth" text="-" bkg="red" />
+      </div>      
+      <div className="row">
+        <RowChild className="row-child forth" text="1" />
+        <RowChild className="row-child forth" text="2" />
+        <RowChild className="row-child forth" text="3" />
+        <RowChild className="row-child forth" text="+" bkg="red" />
+      </div>      
+      <div className="row">
+        <RowChild className="row-child TF" text="0" />
+        <RowChild className="row-child forth" text="=" bkg="red"/>
       </div>      
     </div>
   );
