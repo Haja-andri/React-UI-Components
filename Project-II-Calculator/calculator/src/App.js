f5b2b797437398ba0971fcfd9a6e3adb3abf7253
Haja-andri <haja.andri@gmail.com>
2019-05-28 17:00:27 +0200

    Some adjustment of the style to align with design file

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -7,43 +7,44 @@ function RowChild (props) {
   const { className, text, bkg } = props;
 
   return (
-    <div className={className} style={{backgroundColor: `${bkg}` }} >
+    <div className={className} style={{backgroundColor: `${bkg}`}} >
     {text}
     </div>
   );
 }
 
+
 const App = () => {
   return (
     <div className="mainContainer">
       <div className="row">
-        <RowChild className="row-child full" text="0" />
+        <RowChild className="row-child full" text="0" bkg="black" />
       </div>
       <div className="row">
-        <RowChild className="row-child TF" text="clear" />
-        <RowChild className="row-child forth" text="/" bkg="red"/>
+        <RowChild className="row-child TF small" text="clear" />
+        <RowChild className="row-child forth white" text="/" bkg="red"/>
       </div>      
       <div className="row">
         <RowChild className="row-child forth" text="7" />
         <RowChild className="row-child forth" text="8" />
         <RowChild className="row-child forth" text="9" />
-        <RowChild className="row-child forth" text="X" bkg="red" />
+        <RowChild className="row-child forth white" text="X" bkg="red" />
       </div>      
       <div className="row">
         <RowChild className="row-child forth" text="4" />
         <RowChild className="row-child forth" text="5" />
         <RowChild className="row-child forth" text="6" />
-        <RowChild className="row-child forth" text="-" bkg="red" />
+        <RowChild className="row-child forth white" text="-" bkg="red" />
       </div>      
       <div className="row">
         <RowChild className="row-child forth" text="1" />
         <RowChild className="row-child forth" text="2" />
         <RowChild className="row-child forth" text="3" />
-        <RowChild className="row-child forth" text="+" bkg="red" />
+        <RowChild className="row-child forth white" text="+" bkg="red" />
       </div>      
       <div className="row">
         <RowChild className="row-child TF" text="0" />
-        <RowChild className="row-child forth" text="=" bkg="red"/>
+        <RowChild className="row-child forth white" text="=" bkg="red"/>
       </div>      
     </div>
   );
